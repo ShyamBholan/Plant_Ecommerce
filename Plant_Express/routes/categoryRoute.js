dd7a78ae268419faabe5ac6ac518f835a80b37e4
ShyamBholan <lamashyam474@gmail.com>
2024-05-28 22:13:23 +0545

    updated

--- Plant_Express/routes/categoryRoute.js
+++ Plant_Express/routes/categoryRoute.js
@@ -1,8 +1,12 @@
 const express=require('express')
-const { postCategory, categoryList, categoryDetails } = require('../controllers/categoryController')
+const { postCategory, categoryList, categoryDetails, updateCategory, deleteCategory } = require('../controllers/categoryController')
 const router=express.Router()
 
 router.post('/postcategory',postCategory)
 router.get('/categorylist',categoryList)
-router.get('/categorydetails',categoryDetails)
+router.get('/categorydetails/:id',categoryDetails)
+router.put('/updatecategory/:id',updateCategory)
+router.delete('/deletecategory/:id',deleteCategory)
+
+
 module.exports=router
