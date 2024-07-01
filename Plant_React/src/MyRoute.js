156a45a210cc06080922ccf9a4ced24ea94e5063
ShyamBholan <lamashyam474@gmail.com>
2024-07-01 21:21:23 +0545

    changed

--- Plant_React/src/MyRoute.js
+++ Plant_React/src/MyRoute.js
@@ -8,6 +8,7 @@ import Homepage from './pages/Homepage'
 import AboutUs from './pages/AboutUs'
 import Cart from './pages/Cart'
 import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
 
 const MyRoute = () => {
   return (
@@ -21,6 +22,7 @@ const MyRoute = () => {
             <Route path='login' element={<Login/>}/>
             <Route path='signup' element={<Signup/>}/>
             <Route path='products' element={<Products/>}/>
+            <Route path='productdetails/:productId' element={<ProductDetails/>}/>
             </Route>
             <Route path='*' element={<NotFound/>}/>
         </Routes>
