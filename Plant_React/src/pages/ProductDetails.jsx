dd2eb726a24df2cf16f30ee87875684f2c154a51
ShyamBholan <lamashyam474@gmail.com>
2024-07-05 19:12:32 +0545

    updated

--- Plant_React/src/pages/ProductDetails.jsx
+++ Plant_React/src/pages/ProductDetails.jsx
@@ -21,14 +21,12 @@ const ProductDetails = () => {
             <div className='container bg-primaryy shadow p-5 my-5'>
                 <div className='row d-flex justify-content-around'>
                     <div className='col-md-3'>
-                        <img src={`${IMG_URL}/${product.product_image}`} alt={product.product_name} className='img-fluid'
-                            />
+                        <img src={`${IMG_URL}/${product.product_image}`} alt={product.product_name} className='img-fluid' />
                     </div>
                     <div className='col-md-5'>
                         <h5>{product.product_name}</h5>
                         <h5>Rs.{product.product_price}</h5>
                         <p>{product.product_description}</p>
-                        <br />
                         <br />
                         <button className='btn btn-success' >Add to Cart</button>
                     </div>
