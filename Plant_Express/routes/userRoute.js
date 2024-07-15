7b2f8672625a44cdfb6eb8b861b4f5068e060533
ShyamBholan <lamashyam474@gmail.com>
2024-07-15 18:58:45 +0545

    updated

--- Plant_Express/routes/userRoute.js
+++ Plant_Express/routes/userRoute.js
@@ -1,9 +1,13 @@
 const express=require('express')
-const { postUser, userList, userDetails } = require('../controllers/userController')
+const { postUser, userList, userDetails, postEmailConfirmation, signIn, forgetPassword, resetPassword} = require('../controllers/userController')
 
 const router=express.Router()
 
 router.post('/register',postUser)
+router.put('/confirmation/:token',postEmailConfirmation)
+router.post('/signin',signIn)
+router.post('/forgetpassword',forgetPassword)
+router.put('/resetpassword/:token',resetPassword)
 router.get('/userlist',userList)
 router.get('/userdetail',userDetails)
 
